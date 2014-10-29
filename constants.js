a0f803fec979bf2ced9503e31d3b24b52a2d52de
Aaron Lobo <aaron.lobo@hotmail.com>
2014-10-28 23:18:12 -0700

    Started using heroku environment variables for easier deployment. Left constants file for easy migration to other hosting services

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
 module.exports = Object.freeze({
-    MONGODB_URL: '',
-    YAT_EMAIL: '',
-    WIT_API_KEY: '',
-    GOOGLE_MAPS_API_KEY: '',
-    YELP_CONSUMER_KEY: '',
-	YELP_CONSUMER_SECRET: '',
-	YELP_TOKEN: '',
-	YELP_TOKEN_SECRET: ''
+    MONGODB_URL: process.env.MONGODB_URL,
+    YAT_EMAIL: process.env.YAT_EMAIL,
+    WIT_API_KEY: process.env.WIT_API_KEY,
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    YELP_CONSUMER_KEY: process.env.YELP_CONSUMER_KEY,
+    YELP_CONSUMER_SECRET: process.env.YELP_CONSUMER_SECRET,
+    YELP_TOKEN: process.env.YELP_TOKEN,
+    YELP_TOKEN_SECRET: process.env.YELP_TOKEN_SECRET
 });
